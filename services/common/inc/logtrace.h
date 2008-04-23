91b8a91730aaea46a28e52b4cc804a4a489429cf
Hans Feldt <hans.feldt@ericsson.com>
2008-04-23 10:27:19 +0200

    Introduce logtrace: a small, minimalistic API for logging and tracing

---
+++ services/common/inc/logtrace.h
@@ -0,0 +1,116 @@
+/*      -*- OpenSAF  -*-
+ *
+ * (C) Copyright 2008 The OpenSAF Foundation
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. This file and program are licensed
+ * under the GNU Lesser General Public License Version 2.1, February 1999.
+ * The complete license can be accessed from the following location:
+ * http://opensource.org/licenses/lgpl-license.php
+ * See the Copying file included with the OpenSAF distribution for full
+ * licensing terms.
+ *
+ * Author(s): Ericsson AB
+ *
+ */
+
+/**
+ * This file defines APIs for logging and tracing.
+ * 
+ * Logging is enabled by default and level based. Current backend for logging is
+ * syslog, in the future SAF log could be used for some services (but not all -
+ * boot strapping problem). Filtering is done with the mechanisms of the back
+ * end e.g. the syslog settings.
+ * 
+ * Tracing is disabled by default and category based. Categories are or-ed into
+ * the current mask setting and and-ed with the mask during filtering. Current
+ * backend for tracing is file, in the future DTSv and/or syslog could be used.
+ * Filtering is done by the file back end.
+ */
+
+#ifndef LOGTRACE_H
+#define LOGRACE_H
+
+#include <syslog.h>
+
+/* TRACE categories */
+#define CAT_TRACE       0
+#define CAT_TRACE1      1
+#define CAT_TRACE2      2
+#define CAT_TRACE3      3
+#define CAT_TRACE4      4
+#define CAT_TRACE5      5
+#define CAT_TRACE6      6
+#define CAT_TRACE7      7
+#define CAT_TRACE8      8
+#define CAT_TRACE_ENTER 9
+#define CAT_TRACE_LEAVE 10
+#define CATEGORY_MAX    11
+#define CATEGORY_ALL    0xffffffff
+
+/**
+ * logtrace_init - Initialize the logtrace system.
+ * 
+ * @param ident An identity string to be prepended to every message. Typically
+ * set to the program name.
+ * 
+ * @param pathname The pathname parameter should contain a valid
+ * path name for a file if tracing is to be enabled. The user must have write
+ * access to that file. If the file already exist, it is appended. If the
+ * environment variable is not set or the file name is not valid, no tracing is
+ * performed.
+ * 
+ * @return int - 0 if OK, -1 otherwise
+ */
+extern int logtrace_init(const char *ident, const char *pathname);
+
+/**
+ * trace_category_set - Set the mask used for trace filtering.
+ * 
+ * In libraries the category mask is typically set (to all ones) after a
+ * library specific environment variable has been examined for the name of a
+ * output file. The variable will not exist in a production system hence no
+ * tracing will be done.
+ * 
+ * In daemons there is no need to call this function during initialization,
+ * tracing is disabled by default. In runtime, using e.g. a signal handler the
+ * daemon could call this function to enable or change traced categories.
+ * 
+ * @param category_mask The mask to set, 0 indicates no tracing.
+ * 
+ * @return int - 0 if OK, -1 otherwise
+ */
+extern int trace_category_set(unsigned int category_mask);
+
+/* internal functions, do not use directly */
+extern void _log(const char* file, unsigned int line, int priority,
+                 const char *format, ...) __attribute__((format(printf, 4, 5)));
+extern void _trace(const char* file, unsigned int line, unsigned int category,
+                   const char *format, ...) __attribute__((format(printf, 4, 5)));
+
+/* LOG API. Use same levels as syslog */
+#define LOG_EM(format, args...) _log(__FILE__, __LINE__, LOG_EMERG, (format), ##args)
+#define LOG_AL(format, args...) _log(__FILE__, __LINE__, LOG_ALERT, (format), ##args)
+#define LOG_CR(format, args...) _log(__FILE__, __LINE__, LOG_CRIT, (format), ##args)
+#define LOG_ER(format, args...) _log(__FILE__, __LINE__, LOG_ERR, (format), ##args)
+#define LOG_WA(format, args...) _log(__FILE__, __LINE__, LOG_WARNING, (format), ##args)
+#define LOG_NO(format, args...) _log(__FILE__, __LINE__, LOG_NOTICE, (format), ##args)
+#define LOG_IN(format, args...) _log(__FILE__, __LINE__, LOG_INFO, (format), ##args)
+
+/* TRACE API. */
+#define TRACE(format, args...)   _trace(__FILE__, __LINE__, CAT_TRACE, (format), ##args)
+#define TRACE_1(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE1, (format), ##args)
+#define TRACE_2(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE2, (format), ##args)
+#define TRACE_3(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE3, (format), ##args)
+#define TRACE_4(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE4, (format), ##args)
+#define TRACE_5(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE5, (format), ##args)
+#define TRACE_6(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE6, (format), ##args)
+#define TRACE_7(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE7, (format), ##args)
+#define TRACE_8(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE8, (format), ##args)
+#define TRACE_ENTER()                 _trace(__FILE__, __LINE__, CAT_TRACE_ENTER, "%s ", __FUNCTION__)
+#define TRACE_ENTER2(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE_ENTER, "%s: "format, __FUNCTION__, ##args)
+#define TRACE_LEAVE()                 _trace(__FILE__, __LINE__, CAT_TRACE_LEAVE, "%s ", __FUNCTION__)
+#define TRACE_LEAVE2(format, args...) _trace(__FILE__, __LINE__, CAT_TRACE_LEAVE, "%s: "format, __FUNCTION__, ##args)
+
+#endif
---
+++ services/common/src/logtrace.c
@@ -0,0 +1,131 @@
+/*      -*- OpenSAF  -*-
+ *
+ * (C) Copyright 2008 The OpenSAF Foundation
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. This file and program are licensed
+ * under the GNU Lesser General Public License Version 2.1, February 1999.
+ * The complete license can be accessed from the following location:
+ * http://opensource.org/licenses/lgpl-license.php
+ * See the Copying file included with the OpenSAF distribution for full
+ * licensing terms.
+ *
+ * Author(s): Ericsson AB
+ *
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include <pthread.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include "logtrace.h"
+
+static int trace_fd;
+static int category_mask;
+static char *prefix_name[] = {"EM", "AL", "CR", "ER", "WA", "NO", "IN",
+    "TR", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", ">>", "<<"};
+static const char *my_name;
+
+static void output(const char* file, unsigned int line, int priority, int category,
+                  const char *format, va_list ap)
+{
+    int i, preamble_len;
+    struct timeval tv;
+    char preamble[512];
+    char log_string[1024];
+
+    assert(priority <= LOG_DEBUG && category < CATEGORY_MAX);
+
+    /* Create a nice syslog looking date string */
+    gettimeofday(&tv, NULL);
+    strftime(log_string, sizeof(log_string), "%b %e %k:%M:%S", localtime(&tv.tv_sec));
+    preamble_len = sprintf(preamble, "%s.%06ld %s ", log_string, tv.tv_usec, my_name);
+
+    sprintf(&preamble[preamble_len], "[%ld:%s:%04u] %s %s",
+            syscall(SYS_gettid), file, line, prefix_name[priority + category], format);
+    i = vsprintf(log_string, preamble, ap);
+
+    /* Add line feed if not there already */
+    if (log_string[i - 1] != '\n')
+    {
+        log_string[i] = '\n';
+        log_string[i + 1] = '\0';
+        i++;
+    }
+
+    if (trace_fd != -1)
+    {
+        write(trace_fd, log_string, i);
+
+        /* Do not trace to syslog */
+        if (priority < LOG_DEBUG)
+            syslog(priority, &log_string[preamble_len]);
+    }
+}
+
+void _log(const char* file, unsigned int line, int priority,
+          const char *format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+    output(file, line, priority, 0, format, ap);
+    va_end(ap);
+}
+
+void _trace(const char* file, unsigned int line,
+            unsigned int category, const char *format, ...)
+{
+    va_list ap;
+
+    assert(category < CATEGORY_MAX);
+
+    /* Filter on category */
+    if (!(category_mask & (1 << category)))
+        return;
+
+    va_start(ap, format);
+    output(file, line, LOG_DEBUG, category, format, ap);
+    va_end(ap);
+}
+
+int logtrace_init(const char *ident, const char *pathname)
+{
+    if (ident == NULL || pathname == NULL)
+        return -1;
+
+    my_name = ident;
+    openlog(ident, 0, LOG_LOCAL0);
+
+    trace_fd = open(pathname, O_WRONLY | O_APPEND | O_CREAT,
+        S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (trace_fd == -1)
+        return -1;
+    else
+        return 0;
+}
+
+int trace_category_set(unsigned int category)
+{
+    /* Do not enable tracing without an output device */
+    if (trace_fd == -1)
+        return -1;
+
+    category_mask = category;
+
+    return 0;
+}
+
