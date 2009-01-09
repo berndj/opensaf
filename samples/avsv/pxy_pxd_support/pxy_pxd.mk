ba239360c564f72d2d84af8614ca30c0960d8654
Nagendra Kumar <nagendra.kumar@emerson.com>
2009-01-09 11:13:23 +0530

    Sample for AMF External Component feature support

---
+++ samples/avsv/pxy_pxd_support/pxy_pxd.mk
@@ -0,0 +1,25 @@
+#      -*- OpenSAF  -*-
+#
+# (C) Copyright 2008 The OpenSAF Foundation
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. This file and program are licensed
+# under the GNU Lesser General Public License Version 2.1, February 1999.
+# The complete license can be accessed from the following location:
+# http://opensource.org/licenses/lgpl-license.php
+# See the Copying file included with the OpenSAF distribution for full
+# licensing terms.
+#
+# Author(s): Wind River Systems
+#
+
+pxy_pxd_sources = \
+   samples/avsv/pxy_pxd_support/pxy_pxd_amf.c \
+   samples/avsv/pxy_pxd_support/pxy_pxd_main.c \
+   samples/avsv/pxy_pxd_support/pxy_pxd.h \
+   samples/avsv/pxy_pxd_support/pxy_pxd_inst.sh \
+   samples/avsv/pxy_pxd_support/pxy_pxd_term.sh \
+   samples/avsv/pxy_pxd_support/pxied_clean.sh \
+   samples/avsv/pxy_pxd_support/AppConfig.xml \
+   samples/avsv/pxy_pxd_support/Makefile 
