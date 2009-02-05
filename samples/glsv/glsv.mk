f3cff0cc23e89d3695f41eb16a1e259b2181c337
Jonathan Fournier <jonathan.fournier@windriver.com>
2009-02-05 12:54:52 -0500

    build: remove repeated/included headers in the main makefile

--- samples/glsv/glsv.mk
+++ samples/glsv/glsv.mk
@@ -1,19 +1,3 @@
-#      -*- OpenSAF  -*-
-#
-# (C) Copyright 2008 The OpenSAF Foundation
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE. This file and program are licensed
-# under the GNU Lesser General Public License Version 2.1, February 1999.
-# The complete license can be accessed from the following location:
-# http://opensource.org/licenses/lgpl-license.php
-# See the Copying file included with the OpenSAF distribution for full
-# licensing terms.
-#
-# Author(s): Wind River Systems
-#
-
 glsv_sources = \
    samples/glsv/glsv_demo_app1.c \
    samples/glsv/glsv_main_app1.c \
