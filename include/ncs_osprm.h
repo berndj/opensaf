ce97721555afb02c480c8bd5b35efa8345079d91
Hans Feldt <hans.feldt@ericsson.com>
2009-03-11 14:27:57 +0100

    Use strcspn from string.h (#290)

--- include/ncs_osprm.h
+++ include/ncs_osprm.h
@@ -655,10 +655,6 @@ typedef int32 int64;
 #define NCS_OS_PATH_MAX                    255
 #endif
 
-#ifndef m_NCS_OS_STRCSPN
-#define m_NCS_OS_STRCSPN(s,d)              (int32)strcspn((char *)s,d)
-#endif
-
 /* Caution:  This macro is not THREAD SAFE. 
  * Use sysf_strtok_r for re-entrant functionality
  */
--- include/ncssysf_def.h
+++ include/ncssysf_def.h
@@ -168,8 +168,6 @@ extern LEAPDLL_API int get_char_from_gui();
 #define m_NCS_START(a,b)        m_NCS_OS_START(a,b)
 #define m_NCS_ASSERT(exp)       m_NCS_OS_ASSERT(exp)
 
-#define m_NCS_STRCSPN(s,d)      m_NCS_OS_STRCSPN(s,d)
-
 /* Caution:  This macro is not THREAD SAFE. 
  * Use sysf_strtok_r for re-entrant functionality
  */
