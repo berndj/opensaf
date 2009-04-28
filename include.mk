b9e99ac9b7eb54c38a60599eea0fd0bc019f6835
Jonathan Fournier <jonathan.fournier@windriver.com>
2009-04-28 16:08:37 -0400

    java: update $(javadir)

--- include.mk
+++ include.mk
@@ -2,3 +2,4 @@ pkgsysconfdir = $(sysconfdir)/$(PACKAGE_NAME)
 pkglocalstatedir = $(localstatedir)/lib/$(PACKAGE_NAME)
 docdir = $(datadir)/doc/$(PACKAGE_NAME)-$(PACKAGE_VERSION)
 javadir = $(datadir)/java
+javadocdir = $(datadir)/javadoc
