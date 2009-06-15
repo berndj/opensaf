fc6d9d80234f349bcd875f4f09568f58fff246e2
Jonathan Fournier <jonathan.fournier@windriver.com>
2009-06-15 13:02:15 -0400

    build: cleaned all Makefile.am and introduced Makefile.common

--- include.mk
+++
@@ -1,5 +0,0 @@
-pkgsysconfdir = $(sysconfdir)/$(PACKAGE_NAME)
-pkglocalstatedir = $(localstatedir)/lib/$(PACKAGE_NAME)
-docdir = $(datadir)/doc/$(PACKAGE_NAME)-$(PACKAGE_VERSION)
-javadir = $(datadir)/java
-javadocdir = $(datadir)/javadoc
