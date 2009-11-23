cd73841216a5fee337a80be90b77fdf821179274
Hans Feldt <hans.feldt@ericsson.com>
2009-11-23 10:38:00 +0100

    samples/avsv: add config file for n-way active & README

--- samples/avsv/avsv.mk
+++ samples/avsv/avsv.mk
@@ -1,7 +1,9 @@
 avsv_sources = \
-   samples/avsv/AppConfig.xml \
-   samples/avsv/amf_comp_template.c \
-   samples/avsv/amf_demo.c \
-   samples/avsv/amf_demo_script \
-   samples/avsv/amf_demo_package \
-   samples/avsv/Makefile
+	samples/avsv/AppConfig-2N.xml \
+	samples/avsv/AppConfig-nwayactive.xml \
+	samples/avsv/amf_comp_template.c \
+	samples/avsv/amf_demo.c \
+	samples/avsv/amf_demo_script \
+	samples/avsv/amf_demo_package \
+	samples/avsv/Makefile \
+	samples/avsv/README
