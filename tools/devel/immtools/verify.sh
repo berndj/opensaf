789c7560a5c887b490b8628c9dc14c1db9ecd2dc
Peter Strand <peter.strand@ericsson.com>
2010-02-23 10:09:06 +0100

    decentralized imm.xml - imm.xml no more packaged #700

--- tools/devel/immtools/verify.sh
+++ tools/devel/immtools/verify.sh
@@ -6,25 +6,30 @@
 
 
 
-if [ $# -lt 1 ] ; then
-   echo "usage: $0 generatedImmFile"
+if [ $# -ne 1 ] ; then
+   echo "usage: $0 old_immxml_file"
    exit 1
 fi
 
-RESULT_IMM_FILE=$1
+EXISTING_IMM_FILE=$1
+RESULT_IMM_FILE=../../../../../etc/opensaf/imm.xml
 
 if [ ! -f $1 ] ; then
    echo "The file $1 cannot be found"
    exit 1
 fi
 
-EXISTING_IMM_FILE=../../../../../etc/opensaf/imm.xml
+if [ ! -f $2 ] ; then
+   echo "The file $2 cannot be found"
+   exit 1
+fi
+
 rm -rf ./verify
 mkdir ./verify
 
 
 echo -e "\n\n________________________________________________________"
-echo -e "\n\n Step3 (i.e. only for testing this procedure) compare the generated imm.xml with existing imm.xml ($EXISTING_IMM_FILE) (the generated file may have differences due to that it is more consistent): \n"
+echo -e "\n\n Step3 (i.e. only for testing this procedure) compare the generated imm.xml with the old imm.xml ($EXISTING_IMM_FILE) (the generated file may have differences due to that it is more consistent): \n"
 
 # sort original file (to be able to diff with generated file)
 echo "Sort original imm.xml file as reference (needed for comparision)"
