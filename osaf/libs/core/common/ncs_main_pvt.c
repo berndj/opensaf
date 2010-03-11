5dc6c2a47615d997a657695c629ff469f0579c8d
Jonathan Fournier <jonathan.fournier@windriver.com>
2010-03-11 11:38:22 -0500

    update the glnd daemon scripts, prefix, packages
    Tickets: #256, #288, #654 #658, #855
    
    * introduce common daemon/script prefix
    * Remove ncs_main_pvt deps
    * glnd main uses the daemonzie() call
    * uses a LSB init.d script, ready for NID removal

--- osaf/libs/core/common/ncs_main_pvt.c
+++ osaf/libs/core/common/ncs_main_pvt.c
@@ -232,16 +232,6 @@ static uns32 ncs_d_nd_svr_startup(void)
 	}
 #endif
 
-#if (NCS_GLND == 1)
-	/* Init GLND */
-	m_NCS_DBG_PRINTF("\nGLSV:GLND:ON");
-	if (glnd_lib_req(&lib_create) != NCSCC_RC_SUCCESS) {
-		m_NCS_NID_NOTIFY(NCSCC_RC_FAILURE);
-		printf("GLND lib request failed\n");
-		return m_LEAP_DBG_SINK(NCSCC_RC_FAILURE);
-	}
-#endif
-
 #if (NCS_PDRBD == 1)
 	/* Initialize Pseudo DRBD service */
 	m_NCS_DBG_PRINTF("\nPSEUDO_DRBD:ON\n");
