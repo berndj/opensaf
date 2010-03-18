698d82fb58c58964506c3170c9c7fcd112250b9f
Ingvar Bergström <ingvar.bergstrom@ericsson.com>
2010-03-18 12:22:27 +0100

    SMF sample rolling upgrade campaigns added

--- samples/smfsv/smfsv.mk
+++ samples/smfsv/smfsv.mk
@@ -1,6 +1,4 @@
 smfsv_sources = \
-   samples/smfsv/campaignRollingNodes.xml \
-   samples/smfsv/campaignRollingSU.xml \
    samples/smfsv/offline_install.sh \
    samples/smfsv/offline_remove.sh \
    samples/smfsv/online_install.sh \
