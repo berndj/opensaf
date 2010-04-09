099967bde99c88b46ccf6cfc7786eb77bcf13ef1
Shekhar <chandrasekhar.yadav@emerson.com>
2010-04-09 17:54:42 +0530

    cleanup of infrastructure sample applications (#1037)

--- samples/samples.mk
+++ samples/samples.mk
@@ -1,12 +1,8 @@
 include $(top_srcdir)/samples/avsv/avsv.mk
 include $(top_srcdir)/samples/avsv/pxy_pxd_support/pxy_pxd.mk
 include $(top_srcdir)/samples/cpsv/cpsv.mk
-include $(top_srcdir)/samples/dtsv/dtsv.mk
 include $(top_srcdir)/samples/edsv/edsv.mk
 include $(top_srcdir)/samples/glsv/glsv.mk
-include $(top_srcdir)/samples/leap/leap.mk
-include $(top_srcdir)/samples/mbcsv/mbcsv.mk
-include $(top_srcdir)/samples/mds/mds.mk
 include $(top_srcdir)/samples/mqsv/mqsv.mk
 include $(top_srcdir)/samples/smfsv/smfsv.mk
 
@@ -14,11 +10,7 @@ samples_sources = \
    $(avsv_sources) \
    $(pxy_pxd_sources) \
    $(cpsv_sources) \
-   $(dtsv_sources) \
    $(edsv_sources) \
    $(glsv_sources) \
-   $(leap_sources) \
-   $(mbcsv_sources) \
-   $(mds_sources) \
    $(mqsv_sources) \
    $(smfsv_sources)
