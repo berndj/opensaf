b3c1c4a96ce80b860aa6e3674fbc42a7e41c1e39
Shekar <chandrasekhar.yadav@emerson.com>
2010-04-13 11:07:02 +0530

    Samples makefile fix #1094

--- samples/samples.mk
+++ samples/samples.mk
@@ -1,9 +1,5 @@
 include $(top_srcdir)/samples/avsv/avsv.mk
 include $(top_srcdir)/samples/avsv/pxy_pxd_support/pxy_pxd.mk
-include $(top_srcdir)/samples/cpsv/cpsv.mk
-include $(top_srcdir)/samples/edsv/edsv.mk
-include $(top_srcdir)/samples/glsv/glsv.mk
-include $(top_srcdir)/samples/mqsv/mqsv.mk
 include $(top_srcdir)/samples/smfsv/smfsv.mk
 
 samples_sources = \
