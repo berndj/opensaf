10b7d95bc0db0b2f38b1d147a2c6ed624b190de8
Hans Feldt <hans.feldt@ericsson.com>
2011-08-02 14:19:05 +0200

    leap: assert removed from ncs_ipc_send (#1908)

--- osaf/libs/core/leap/sysf_ipc.c
+++ osaf/libs/core/leap/sysf_ipc.c
@@ -527,8 +527,6 @@ uint32_t ncs_ipc_send(SYSF_MBX *mbx, NCS_IPC_MSG *msg, NCS_IPC_PRIORITY prio)
 	m_NCS_SET_ST_QLAT();
 
 	if (ipc_enqueue_ind_processing(ncs_ipc, queue_number) != NCSCC_RC_SUCCESS) {
-		/* Should never reach here */
-		assert(0);
 		m_NCS_UNLOCK(&ncs_ipc->queue_lock, NCS_LOCK_WRITE);
 		ncshm_give_hdl((uint32_t)*mbx);
 		return NCSCC_RC_FAILURE;
