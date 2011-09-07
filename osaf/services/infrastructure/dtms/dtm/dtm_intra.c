90848baadd52560c549a957d836d1b6aad9154e4
Praveen <pmalviya@goahead.com>
2011-09-07 23:57:16 +0530

    Read-Write permissions for 'others' for MDS tcp server socket (#2039)

--- osaf/services/infrastructure/dtms/dtm/dtm_intra.c
+++ osaf/services/infrastructure/dtms/dtm/dtm_intra.c
@@ -185,7 +185,7 @@ uint32_t dtm_intra_processing_init(void)
 			return NCSCC_RC_FAILURE;
 		}
 
-		if (chmod(UX_SOCK_NAME_PREFIX, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)) < 0) { 
+		if (chmod(UX_SOCK_NAME_PREFIX, (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH)) < 0) { 
 			LOG_ER("chmod %s failed - %s", UX_SOCK_NAME_PREFIX, strerror(errno));
 			close(dtm_intranode_cb->server_sockfd);
 			free(dtm_intranode_cb);
