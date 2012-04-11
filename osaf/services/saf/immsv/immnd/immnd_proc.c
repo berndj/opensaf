b757f6bd0bba66558198d9c80c69e4ca178d7e72
Anders Bjornerstedt <anders.bjornerstedt@ericsson.com>
2012-04-11 13:41:21 +0200

    Immsv: SyncRequested flag reverted back to on when failure to send to IMMD (#2617)

--- osaf/services/saf/immsv/immnd/immnd_proc.c
+++ osaf/services/saf/immsv/immnd/immnd_proc.c
@@ -557,6 +557,7 @@ static uint32_t immnd_announceSync(IMMND_CB *cb, SaUint32T newEpoch)
 
 	if (rc != NCSCC_RC_SUCCESS) {
 		LOG_ER("Coord failed to send start of sync to IMMD");
+		cb->mSyncRequested = true; /* revert sync request marker back to on */
 		return 0;
 	}
 
