a3306d9eb537e6e583458a252e93b6048b2e5c1a
Bertil Engelholm <bertil.engelholm@ericsson.com>
2013-02-14 12:15:32 +0100

    smfsv: finalize search handle to avoid leaks (#3004)

--- osaf/services/saf/smfsv/smfd/SmfUtils.cc
+++ osaf/services/saf/smfsv/smfd/SmfUtils.cc
@@ -374,6 +374,7 @@ SmfImmUtils::getChildren(const std::string & i_dn, std::list < std::string > &o_
 	}
 
 done:
+        (void) immutil_saImmOmSearchFinalize(immSearchHandle);
 	TRACE_LEAVE();
 	return rc;
 }
