acaf4ed6f20d89cab14cfaa5cbdbe4ba05332844
Nagendra Kumar <nagendra.k@oracle.com>
2013-03-22 16:35:20 +0530

    avsv: Correct assignment counters during node lock operation (#3069)

--- osaf/services/saf/avsv/avd/avd_node.c
+++ osaf/services/saf/avsv/avd/avd_node.c
@@ -844,8 +844,9 @@ void avd_node_admin_lock_unlock_shutdown(AVD_AVND *node,
 				avd_sg_app_su_inst_func(cb, su->sg_of_su);
 			}
 
-			/* check if admin lock has caused a SG REALIGN to send admin cbk */
-			if (su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SG_REALIGN)
+			/* Update the counter to respond adm opreration. Checking list_of_susi is required to obviate
+			   incrementing counters for spare Sus on the node.*/
+			if ((su->list_of_susi != NULL) && (su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SG_REALIGN))
 				node->su_cnt_admin_oper++;
 
 			/* get the next SU on the node */
@@ -946,16 +947,18 @@ void avd_node_admin_lock_unlock_shutdown(AVD_AVND *node,
 		/* Now call the SG FSM for each of the SUs that have SI assignment. */
 		su = node->list_of_su;
 		while (su != NULL) {
+			is_assignments_done = false;
 			avd_su_readiness_state_set(su, SA_AMF_READINESS_OUT_OF_SERVICE);
 			if (su->list_of_susi != AVD_SU_SI_REL_NULL) {
+				is_assignments_done = true;
 				su->sg_of_su->su_admin_down(cb, su, node);
 			}
 
 			/* since an SU is now OOS we need to take a relook at the SG. */
 			avd_sg_app_su_inst_func(cb, su->sg_of_su);
 
-			if (su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SG_REALIGN ||
-			    su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SU_OPER)
+			if ((is_assignments_done) && ((su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SG_REALIGN) ||
+						(su->sg_of_su->sg_fsm_state == AVD_SG_FSM_SU_OPER)))
 				node->su_cnt_admin_oper++;
 
 			/* get the next SU on the node */
