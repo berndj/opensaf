7ef6a195d39aec015dd49a02ae91fae935020fd0
Hans Feldt <hans.feldt@ericsson.com>
2013-08-28 08:08:21 +0200

    amf: remove avsv tests [#533]

--- tests/avsv/suites/comp_inst.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#source /opt/opensaf/tetware/lib_path.sh
-export TET_ROOT=/usr/local/tet
-export TET_TMP_DIR=/tmp
-export PATH=$PATH:/usr/local/tet:/usr/local/tet/bin:/usr/bin/X11/xterm
-
-tcc -e -s $TET_BASE_DIR/avsv/suites/test_scenario $TET_BASE_DIR/avsv/suites &
-exit 0
