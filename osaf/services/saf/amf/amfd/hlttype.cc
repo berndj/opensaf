d2a004c59006cda01656e34f2947dbc6abf73da8
Hans Feldt <hans.feldt@ericsson.com>
2014-02-06 11:29:54 +0100

    amfd: allow changes to hctype [#539]

--- osaf/services/saf/amf/amfd/hlttype.cc
+++ osaf/services/saf/amf/amfd/hlttype.cc
@@ -15,12 +15,46 @@
  *
  */
 
-#include <stddef.h>
+#include <string.h>
 
 #include <logtrace.h>
 #include <immutil.h>
 #include <ncsgl_defs.h>
 #include <imm.h>
+
+/**
+ * Validates proposed change in comptype
+ */
+static SaAisErrorT ccb_completed_modify_hdlr(const CcbUtilOperationData_t *opdata)
+{
+	SaAisErrorT rc = SA_AIS_OK;
+	const SaImmAttrModificationT_2 *mod;
+	int i = 0;
+	const char *dn = (char*)opdata->objectName.value;
+
+	while ((mod = opdata->param.modify.attrMods[i++]) != NULL) {
+		if (strcmp(mod->modAttr.attrName, "saAmfHctDefPeriod") == 0) {
+			SaTimeT value = *((SaTimeT *)mod->modAttr.attrValues[0]);
+			if (value < SA_TIME_ONE_SECOND) {
+				report_ccb_validation_error(opdata,
+					"Invalid saAmfHctDefPeriod for '%s'", dn);
+				rc = SA_AIS_ERR_BAD_OPERATION;
+				goto done;
+			}
+		} else if (strcmp(mod->modAttr.attrName, "saAmfHctDefMaxDuration") == 0) {
+			SaTimeT value = *((SaTimeT *)mod->modAttr.attrValues[0]);
+			if (value < 100 * SA_TIME_ONE_MILLISECOND) {
+				report_ccb_validation_error(opdata,
+					"Invalid saAmfHctDefMaxDuration for '%s'", dn);
+				rc = SA_AIS_ERR_BAD_OPERATION;
+				goto done;
+			}
+		}
+	}
+
+done:
+	return rc;
+}
 
 static SaAisErrorT hct_ccb_completed_cb(CcbUtilOperationData_t *opdata)
 {
@@ -33,7 +67,7 @@ static SaAisErrorT hct_ccb_completed_cb(CcbUtilOperationData_t *opdata)
 		rc = SA_AIS_OK;
 		break;
 	case CCBUTIL_MODIFY:
-		report_ccb_validation_error(opdata, "Modification of SaAmfHealthcheckType not supported");
+		rc = ccb_completed_modify_hdlr(opdata);
 		break;
 	case CCBUTIL_DELETE:
 		rc = SA_AIS_OK;
@@ -55,6 +89,9 @@ static void hct_ccb_apply_cb(CcbUtilOperationData_t *opdata)
 		break;
 	case CCBUTIL_DELETE:
 		break;
+	case CCBUTIL_MODIFY:
+		// values not used, no need to reinitialize type
+		break;
 	default:
 		osafassert(0);
 		break;
@@ -63,6 +100,7 @@ static void hct_ccb_apply_cb(CcbUtilOperationData_t *opdata)
 
 void avd_hctype_constructor(void)
 {
-	avd_class_impl_set(const_cast<SaImmClassNameT>("SaAmfHealthcheckType"), NULL, NULL, hct_ccb_completed_cb, hct_ccb_apply_cb);
+	avd_class_impl_set(const_cast<SaImmClassNameT>("SaAmfHealthcheckType"),
+			NULL, NULL, hct_ccb_completed_cb, hct_ccb_apply_cb);
 }
 
