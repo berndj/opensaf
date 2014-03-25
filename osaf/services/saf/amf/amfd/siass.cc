cf2f0c263d9de6322221988c4777c3412196f0ae
Hans Feldt <hans.feldt@ericsson.com>
2014-03-25 12:57:21 +0100

    amfd: send notification when SI first assigned to SU [#544]

--- osaf/services/saf/amf/amfd/siass.cc
+++ osaf/services/saf/amf/amfd/siass.cc
@@ -255,10 +255,8 @@ AVD_SU_SI_REL *avd_susi_create(AVD_CL_CB *cb, AVD_SI *si, AVD_SU *su, SaAmfHASta
 done:
 	if ((ckpt == false) && (su_si != NULL)) {
 		avd_create_susi_in_imm(state, &si->name, &su->name);
-		saflog(LOG_NOTICE, amfSvcUsrName, "HA State %s of %s for %s",
-			   avd_ha_state[state], su->name.value, si->name.value);
-
 		avd_susi_update_assignment_counters(su_si, AVSV_SUSI_ACT_ASGN, state, state);
+		avd_gen_su_ha_state_changed_ntf(cb, su_si);
 	}
 
 	TRACE_LEAVE();
