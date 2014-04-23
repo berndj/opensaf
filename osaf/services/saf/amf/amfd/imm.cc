79b1f2687eafbc6766e281ccb7d95e7de75e8b38
Hans Feldt <hans.feldt@ericsson.com>
2014-04-23 17:17:17 +0200

    amfd: fix imm job update [#868]
    Ugly logs from amfd like:
    Apr 19 19:55:18 SC-1 osafamfd[469]: ER exec: update FAILED 12
    
    When an IMM update job is executed and for some reason the object does not exist,
    the job remains in the queue. Reason is because the wrong error code is checked.
    
    By correcting the check, the job is removed and no logging appears.

--- osaf/services/saf/amf/amfd/imm.cc
+++ osaf/services/saf/amf/amfd/imm.cc
@@ -150,7 +150,7 @@ AvdJobDequeueResultT ImmObjUpdate::exec(SaImmOiHandleT immOiHandle)
 
 	rc = saImmOiRtObjectUpdate_2(immOiHandle, &dn_, attrMods);
 
-	if ((rc == SA_AIS_OK) || (rc == SA_AIS_ERR_EXIST)) {
+	if ((rc == SA_AIS_OK) || (rc == SA_AIS_ERR_NOT_EXIST)) {
 		delete Fifo::dequeue();
 		res = JOB_EXECUTED;
 	} else if (rc == SA_AIS_ERR_TRY_AGAIN) {
