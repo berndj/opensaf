7b5d86bc5ee348796d5cd9b921d501b588a4da14
Neelakanta Reddy <reddy.neelakanta@oracle.com>
2016-10-31 10:24:03 +0530

    smf:read imm longdn attribute at campaigninit [#2119]

--- osaf/services/saf/smfsv/smfd/SmfCampaignInit.cc
+++ osaf/services/saf/smfsv/smfd/SmfCampaignInit.cc
@@ -228,8 +228,14 @@ SmfCampaignInit::execute()
 	std::list < SmfUpgradeAction * >::iterator upActiter;
 	upActiter = m_campInitAction.begin();
 	while (upActiter != m_campInitAction.end()) {
-       		SaAisErrorT rc = (*upActiter)->execute(SmfCampaignThread::instance()->getImmHandle(),
-                                                       &initRollbackDn);
+		TRACE("3. %s: read_IMM_long_DN_config_and_set_control_block()",__FUNCTION__);
+		if (!immUtil.read_IMM_long_DN_config_and_set_control_block(smfd_cb)) {
+			LOG_ER("SmfCampaignInit: reading long DN config from IMM FAILED");
+			TRACE_LEAVE();
+			return false;
+		}
+		SaAisErrorT rc = (*upActiter)->execute(SmfCampaignThread::instance()->getImmHandle(),
+				&initRollbackDn);
 		if (rc != SA_AIS_OK) {
 			LOG_ER("SmfCampaignInit init action %d failed, rc=%s", (*upActiter)->getId(), saf_error(rc));
 			TRACE_LEAVE();
