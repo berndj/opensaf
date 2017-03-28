2f8d92c3e1f11654b1dd833117fa6a0583483cdf
Neelakanta Reddy <reddy.neelakanta@oracle.com>
2017-03-28 16:30:11 +0530

    imm:retry ccbabort when failed with TRY_AGAIN and TIMEOUT[#2398]

--- src/imm/agent/imma_om_api.cc
+++ src/imm/agent/imma_om_api.cc
@@ -9144,6 +9144,7 @@ static SaAisErrorT imma_finalizeCcb(SaImmCcbHandleT ccbHandle, bool keepCcbHandl
 		} else {
 			/* TRY_AGAIN or TIMEOUT => allow user to try finalize again. */
 			ccb_node->mExclusive = false;
+			goto done;
 		}
 		if(keepCcbHandleOpen) { /* saImmOmCcbAbort */
 			ccb_node->mApplied = true;
