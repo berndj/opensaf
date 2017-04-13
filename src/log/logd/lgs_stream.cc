95a78ab0bc72df2250ff80f161aff65c609f4c35
Canh Van Truong <canh.v.truong@dektech.com.au>
2017-04-13 14:11:22 +0200

    log: fix race condition in share file system [#2383]
    The issues:
    It's happen because when switchover, active side switch to quiesced. For short period
    (MDS's quiesced acknowledgment time), the log server in quiesced state may still get
    request from log agent if these request already put to mds queue. if the request are open
    stream or write async, the log file is opened and is not closed in switchover processing
    any more when it up to standby. An other side that switch from standby to active state
    may also open the same log file name. This cause when delete cfg app stream, closing,
    rename log file will happen in both active and standby. And just rename in active side is
    successfull, in standby will be failed
    
    Fix:
    
    Return TRY AGAIN for the requests API in transition condition (quiesced state)
    
    There are also converting LOG_ER to LOG_WA if renaming log file and get error return
    from file handle thread.

--- src/log/logd/lgs_stream.cc
+++ src/log/logd/lgs_stream.cc
@@ -840,7 +840,7 @@ void log_stream_close(log_stream_t **s, time_t *close_time_ptr) {
       }
 
       if (rc == -1) {
-        LOG_ER("Could not rename log file: %s", strerror(errno));
+        LOG_WA("Could not rename log file: %s", strerror(errno));
         goto done_files;
       }
 
@@ -854,7 +854,7 @@ void log_stream_close(log_stream_t **s, time_t *close_time_ptr) {
                                timeString, LGS_LOG_FILE_CONFIG_EXT, emptyStr);
       }
       if (rc == -1) {
-        LOG_ER("Could not rename config file: %s", strerror(errno));
+        LOG_WA("Could not rename config file: %s", strerror(errno));
         goto done_files;
       }
     }
