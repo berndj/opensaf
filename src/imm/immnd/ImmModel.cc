b806280ba7990a39382051ae961029ec7deec637
Zoran Milinkovic <zoran.milinkovic@ericsson.com>
2017-06-14 11:27:40 +0200

    imm: fix counting timeouts for old critical CCBs [#2495]
    The patch fix counting timeouts for old critical CCBs

--- src/imm/immnd/ImmModel.cc
+++ src/imm/immnd/ImmModel.cc
@@ -14240,7 +14240,7 @@ void ImmModel::getOldCriticalCcbs(IdVector& cv, SaUint32T* pbeConnPtr,
       }
 
       if ((ccb->mPbeRestartId == 0) &&
-          osaf_timer_is_expired_sec(&now, &ccb->mWaitStartTime,
+          !osaf_timer_is_expired_sec(&now, &ccb->mWaitStartTime,
                                     (DEFAULT_TIMEOUT_SEC + addSecs))) {
         timespec elapsed;
         osaf_timespec_subtract(&now, &ccb->mWaitStartTime, &elapsed);
