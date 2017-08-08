890ea747804f61103af5be1a586631b3c0563942
Anders Widell <anders.widell@ericsson.com>
2017-08-08 10:54:22 +0200

    base: Fix GCC 7.1.1 build error [#2535]
    Fix the following build error when using GCC 7.1.1:
    
    In file included from /home/opensaf/osaf_test_workdir/srcdir/opensaf-5.17.10/src/base/tests/osaf_timespec_average_test.cc:19:0:
    /home/opensaf/osaf_test_workdir/googletest/googletest/include/gtest/gtest.h: In instantiation of ‘testing::AssertionResult testing::internal::CmpHelperEQ(const char*, const char*, const T1&, const T2&) [with T1 = long int; T2 = long unsigned int]’:
    /home/opensaf/osaf_test_workdir/googletest/googletest/include/gtest/gtest.h:1421:23:   required from ‘static testing::AssertionResult testing::internal::EqHelper<lhs_is_null_literal>::Compare(const char*, const char*, const T1&, const T2&) [with T1 = long int; T2 = long unsigned int; bool lhs_is_null_literal = false]’
    /home/opensaf/osaf_test_workdir/srcdir/opensaf-5.17.10/src/base/tests/osaf_timespec_average_test.cc:38:3:   required from here
    /home/opensaf/osaf_test_workdir/googletest/googletest/include/gtest/gtest.h:1392:11: error: comparison between signed and unsigned integer expressions [-Werror=sign-compare]
       if (lhs == rhs) {
           ~~~~^~~~~~
    cc1plus: all warnings being treated as errors
      CXX      src/amf/amfd/tests/bin_testamfd-test_amfdb.o

--- src/base/tests/osaf_timespec_average_test.cc
+++ src/base/tests/osaf_timespec_average_test.cc
@@ -18,7 +18,8 @@
 #include "base/osaf_time.h"
 #include "gtest/gtest.h"
 
-static const uint64_t NANOS_PER_SEC = kNanosPerSec;
+static const uint64_t NANOS_PER_SEC_U64 = kNanosPerSec;
+static const long NANOS_PER_SEC_LONG = kNanosPerSec;
 
 TEST(OsafTimespecAverage, Zero) {
   struct timespec time1 = {0, 0};
@@ -35,24 +36,24 @@ TEST(OsafTimespecAverage, OneSecond) {
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, 0);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC / 2);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG / 2);
 }
 
 TEST(OsafTimespecAverage, AlmostOneSecond) {
-  struct timespec time1 = {0, NANOS_PER_SEC - 1};
-  struct timespec time2 = {0, NANOS_PER_SEC - 1};
+  struct timespec time1 = {0, NANOS_PER_SEC_LONG - 1};
+  struct timespec time2 = {0, NANOS_PER_SEC_LONG - 1};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, 0);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC - 1);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG - 1);
 }
 
 TEST(OsafTimespecAverage, AlmostTwoSeconds) {
-  struct timespec time1 = {1, NANOS_PER_SEC - 1};
-  struct timespec time2 = {0, NANOS_PER_SEC - 1};
+  struct timespec time1 = {1, NANOS_PER_SEC_LONG - 1};
+  struct timespec time2 = {0, NANOS_PER_SEC_LONG - 1};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
-  uint64_t expected_ns = (NANOS_PER_SEC + 2 * (NANOS_PER_SEC - 1)) / 2;
+  uint64_t expected_ns = (NANOS_PER_SEC_U64 + 2 * (NANOS_PER_SEC_U64 - 1)) / 2;
   struct timespec expected;
   osaf_nanos_to_timespec(expected_ns, &expected);
   EXPECT_EQ(average.tv_sec, expected.tv_sec);
@@ -65,7 +66,7 @@ TEST(OsafTimespecAverage, SimpleCase) {
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   uint64_t expected_ns =
-      (222 * NANOS_PER_SEC + 111 * NANOS_PER_SEC + 1234 + 5678) / 2;
+      (222 * NANOS_PER_SEC_U64 + 111 * NANOS_PER_SEC_U64 + 1234 + 5678) / 2;
   struct timespec expected;
   osaf_nanos_to_timespec(expected_ns, &expected);
   EXPECT_EQ(average.tv_sec, expected.tv_sec);
@@ -73,12 +74,12 @@ TEST(OsafTimespecAverage, SimpleCase) {
 }
 
 TEST(OsafTimespecAverage, NanosOverflow) {
-  struct timespec time1 = {10, NANOS_PER_SEC / 2};
-  struct timespec time2 = {11, NANOS_PER_SEC / 2};
+  struct timespec time1 = {10, NANOS_PER_SEC_LONG / 2};
+  struct timespec time2 = {11, NANOS_PER_SEC_LONG / 2};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   uint64_t expected_ns =
-      (10 * NANOS_PER_SEC + 11 * NANOS_PER_SEC + NANOS_PER_SEC) / 2;
+      (10 * NANOS_PER_SEC_U64 + 11 * NANOS_PER_SEC_U64 + NANOS_PER_SEC_U64) / 2;
   struct timespec expected;
   osaf_nanos_to_timespec(expected_ns, &expected);
   EXPECT_EQ(average.tv_sec, expected.tv_sec);
@@ -88,29 +89,29 @@ TEST(OsafTimespecAverage, NanosOverflow) {
 TEST(OsafTimespecAverage, Max) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
-  struct timespec time2 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
+  struct timespec time2 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, max_time);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC - 1);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG - 1);
 }
 
 TEST(OsafTimespecAverage, MaxAndZero) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec time2 = {0, 0};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, max_time / 2);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC - 1);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG - 1);
 }
 
 TEST(OsafTimespecAverage, MaxAndOneNanoSecond) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec time2 = {0, 1};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
@@ -121,7 +122,7 @@ TEST(OsafTimespecAverage, MaxAndOneNanoSecond) {
 TEST(OsafTimespecAverage, MaxAndTwoNanoSeconds) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec time2 = {0, 2};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
@@ -132,21 +133,21 @@ TEST(OsafTimespecAverage, MaxAndTwoNanoSeconds) {
 TEST(OsafTimespecAverage, MaxAndOneSecond) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec time2 = {1, 0};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, max_time / 2 + 1);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC / 2 - 1);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG / 2 - 1);
 }
 
 TEST(OsafTimespecAverage, MaxAndTwoSeconds) {
   time_t max_time = INT32_MAX;
   if (sizeof(time_t) == 8) max_time = INT64_MAX;
-  struct timespec time1 = {max_time, NANOS_PER_SEC - 1};
+  struct timespec time1 = {max_time, NANOS_PER_SEC_LONG - 1};
   struct timespec time2 = {2, 0};
   struct timespec average = {1, 1};
   osaf_timespec_average(&time1, &time2, &average);
   EXPECT_EQ(average.tv_sec, max_time / 2 + 1);
-  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC - 1);
+  EXPECT_EQ(average.tv_nsec, NANOS_PER_SEC_LONG - 1);
 }
