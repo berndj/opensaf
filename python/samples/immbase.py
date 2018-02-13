aba704b9a4bed3aafa269ead16d264554ca505e3
Danh Vo <danh.c.vo@dektech.com.au>
2018-02-13 16:54:59 +0700

    pyosaf: refactor IMM samples to use new pyosaf utils [#2681]

--- python/samples/immbase.py
+++ python/samples/immbase.py
@@ -1,6 +1,7 @@
 ############################################################################
 #
 # (C) Copyright 2011 The OpenSAF Foundation
+# (C) Copyright 2018 Ericsson AB. All rights reserved.
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -12,80 +13,89 @@
 # licensing terms.
 #
 # Author(s): Wind River Systems, Inc.
+#            Ericsson AB
 #
 ############################################################################
-
+# pylint: disable=fixme
 from pyosaf import saImm
-from pyosaf.saImmOm import *
-from pyosaf.saAmf import *
+from pyosaf.saAis import eSaAisErrorT, unmarshalNullArray
+from pyosaf.saAmf import SaVersionT, eSaAmfPresenceStateT, \
+    eSaAmfReadinessStateT, eSaAmfOperationalStateT, eSaAmfAdminStateT, \
+    eSaAmfHAStateT, eSaAmfAssignmentStateT
+
 
 IMM_VERSION = SaVersionT('A', 2, 1)
 
+
 class SafException(Exception):
-	def __init__(self, value):
-		self.value = value
-	def __str__(self):
-		return eSaAisErrorT.whatis(self.value)
+    """ SafException class """
+    def __init__(self, value):
+        self.value = value
+        Exception.__init__(value)
+
+    def __str__(self):
+        return eSaAisErrorT.whatis(self.value)
+
 
 class SafObject(object):
-	resolver = {
-		# SaAmfComp
-			'saAmfCompPresenceState': eSaAmfPresenceStateT,
-			'saAmfCompReadinessState': eSaAmfReadinessStateT,
-			'saAmfCompOperState': eSaAmfOperationalStateT,
-		# SaAmfNode
-			'saAmfNodeOperState': eSaAmfOperationalStateT,
-			'saAmfNodeAdminState': eSaAmfAdminStateT,
-		# SaAmfCSIAssignment
-			'saAmfCSICompHAReadinessState': eSaAmfReadinessStateT,
-			'saAmfCSICompHAState': eSaAmfHAStateT,
-		# SaAmfSIAssignment
-			'saAmfSISUHAReadinessState': eSaAmfReadinessStateT,
-			'saAmfSISUHAState': eSaAmfHAStateT,
-		# SaAmfApplication
-			'saAmfApplicationAdminState': eSaAmfAdminStateT,
-		# SaClmNode
-			'saClmNodeAdminState': eSaAmfAdminStateT,
-		# SaAmfSU
-			'saAmfSUPresenceState': eSaAmfPresenceStateT,
-			'saAmfSUReadinessState': eSaAmfReadinessStateT,
-			'saAmfSUAdminState': eSaAmfAdminStateT,
-			'saAmfSUOperState': eSaAmfOperationalStateT,
-		# SaAmfSG
-			'saAmfSGAdminState': eSaAmfAdminStateT,
-		# SaAmfSI
-			'saAmfSIAdminState': eSaAmfAdminStateT,
-			'saAmfSIAssignmentState': eSaAmfAssignmentStateT,
-		# SaAmfCluster
-			'saAmfClusterAdminState': eSaAmfAdminStateT,
-		# TODO: Once we create saSmf.py
-		# SaSmfProcedure
-		# SaSmfStep
-		# SaSmfCampaign
-	}
+    """ SafObject class """
+    resolver = {
+        # SaAmfComp
+        'saAmfCompPresenceState': eSaAmfPresenceStateT,
+        'saAmfCompReadinessState': eSaAmfReadinessStateT,
+        'saAmfCompOperState': eSaAmfOperationalStateT,
+        # SaAmfNode
+        'saAmfNodeOperState': eSaAmfOperationalStateT,
+        'saAmfNodeAdminState': eSaAmfAdminStateT,
+        # SaAmfCSIAssignment
+        'saAmfCSICompHAReadinessState': eSaAmfReadinessStateT,
+        'saAmfCSICompHAState': eSaAmfHAStateT,
+        # SaAmfSIAssignment
+        'saAmfSISUHAReadinessState': eSaAmfReadinessStateT,
+        'saAmfSISUHAState': eSaAmfHAStateT,
+        # SaAmfApplication
+        'saAmfApplicationAdminState': eSaAmfAdminStateT,
+        # SaClmNode
+        'saClmNodeAdminState': eSaAmfAdminStateT,
+        # SaAmfSU
+        'saAmfSUPresenceState': eSaAmfPresenceStateT,
+        'saAmfSUReadinessState': eSaAmfReadinessStateT,
+        'saAmfSUAdminState': eSaAmfAdminStateT,
+        'saAmfSUOperState': eSaAmfOperationalStateT,
+        # SaAmfSG
+        'saAmfSGAdminState': eSaAmfAdminStateT,
+        # SaAmfSI
+        'saAmfSIAdminState': eSaAmfAdminStateT,
+        'saAmfSIAssignmentState': eSaAmfAssignmentStateT,
+        # SaAmfCluster
+        'saAmfClusterAdminState': eSaAmfAdminStateT,
+        # TODO: Once we create saSmf.py
+        # SaSmfProcedure
+        # SaSmfStep
+        # SaSmfCampaign
+    }
 
-	@staticmethod
-	def resolveStates(attribs):
-		for (attr, vals) in attribs.iteritems():
-			enum = SafObject.resolver.get(attr)
-			if enum:
-				vals[1] = [enum.whatis(val) for val in vals[1]]
+    @staticmethod
+    def resolveStates(attribs):
+        for (attr, vals) in attribs.iteritems():
+            enum = SafObject.resolver.get(attr)
+            if enum:
+                vals[1] = [enum.whatis(val) for val in vals[1]]
 
-	@staticmethod
-	def serialize(entry):
-		return entry.attribs
+    @staticmethod
+    def serialize(entry):
+        return entry.attribs
 
-	def __init__(self, dn, attribs, numeric=False):
-		self.dn = dn
-		self.attribs = {}
-		attrList = unmarshalNullArray(attribs)
-		for attr in attrList:
-			attrRange = range(attr.attrValuesNumber)
-			self.attribs[attr.attrName] = [
-				saImm.eSaImmValueTypeT.whatis(attr.attrValueType),
-				[saImm.unmarshalSaImmValue(
-					attr.attrValues[val],
-					attr.attrValueType) for val in attrRange]
-				]
-		if not numeric:
-			SafObject.resolveStates(self.attribs)
+    def __init__(self, dn, attribs, numeric=False):
+        self.dn = dn
+        self.attribs = {}
+        attrList = unmarshalNullArray(attribs)
+        for attr in attrList:
+            attrRange = range(attr.attrValuesNumber)
+            self.attribs[attr.attrName] = [
+                saImm.eSaImmValueTypeT.whatis(attr.attrValueType),
+                [saImm.unmarshalSaImmValue(
+                    attr.attrValues[val],
+                    attr.attrValueType) for val in attrRange]]
+        if not numeric:
+            SafObject.resolveStates(self.attribs)
